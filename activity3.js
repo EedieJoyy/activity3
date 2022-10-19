d7ed64f820a90057c53c5329bcf0a9a23b999322
EedieJoyy <111791658+EedieJoyy@users.noreply.github.com>
2022-10-19 22:00:34 +0800

    Add files via upload

---
+++ activity3.js
@@ -0,0 +1,4 @@
+function alph_order(str) {
+    return str.split("").sort().join("")
+}console.log(alph_order("eddiejoydampiganon"))
+ console.log(alph_order("bachelor of science in information technology"))
